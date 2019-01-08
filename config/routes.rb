12d7bbeb025a851f2d5aa5b504b24083d543e107
proxyz <mr.prxz@gmail.com>
2019-01-08 23:30:44 +0200

    Clear unused action and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments, only: [:create, :index, :destroy]
+  resources :comments, only: [:create]
   resources :categories, :path => '/'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
