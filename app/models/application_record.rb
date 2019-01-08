421292211e2f15f424a8deab282017cc3d83cb04
proxyz <mr.prxz@gmail.com>
2019-01-08 23:25:32 +0200

    Add validations to comments

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,7 +3,15 @@ class ApplicationRecord < ActiveRecord::Base
 end
 class NameValidator < ActiveModel::Validator
   def validate(record)
-    if ( !record.name.match(/[A-Z.]{1}[a-z.]{1,} [A-Za-z.]{2,}/)  )
+    if ( !record.name.match(/[A-Z]{1}[a-z.]{1,} [A-Za-z.]{2,}/)  )
+      record.errors[:base] << "Incorrect name"
+    end
+  end
+end
+
+class AuthorValidator < ActiveModel::Validator
+  def validate(record)
+    if ( !record.author.match(/[A-Z]{1}[a-z]{1,} [A-Z]{1}[A-Za-z.]{1,}/)  )
       record.errors[:base] << "Incorrect name"
     end
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :parent, polymorphic: true
+  validates :content, presence: true
+  validates_with AuthorValidator
 end
